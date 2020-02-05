b8e0d9831b84adc7a6349f5d66f754fa94a87947
Irek.G <azpirin123z@yandex.ru>
2020-02-05 23:34:35 +0300

    step2

--- src/Cli.php
+++ src/Cli.php
@@ -2,12 +2,15 @@
 
 namespace BrainGames\Cli;
 
-use function cli\line;
-use function cli\prompt;
+
+
+use function \cli\line;
+use function \cli\prompt;
 
 function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line('Hello, %s!', $name);
+    line("Hello, %s!", $name);
+echo 'irek';
 }
--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'be01b9b16925dcb22165c40b46681ac6' => $vendorDir . '/wp-cli/php-cli-tools/lib/cli/cli.php',
+    '113dde06f802987e950d857215a889ad' => $baseDir . '/src/Cli.php',
 );
