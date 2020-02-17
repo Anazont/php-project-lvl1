72338fdbecd936eb6906f02fea9bd7c881aaddf1
Irek Gaysin <azpirin123z>
2020-02-17 11:31:36 +0300

    brain-calc

---
+++ src/games/calc.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BrainGames\Games\Calc;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "What is the result of the expression?";
+const OPERATIONS = ['+', '-', '*'];
+const MAX_RANDOM_VALUE = 99;
+
+/**
+ * Function calculate two numbers
+ *
+ * @param integer $num1      first value
+ * @param integer $num2      second value
+ * @param string  $operation type of operation
+ *
+ * @return integer
+ */
+function calculate($num1, $num2, $operation)
+{
+    switch ($operation) {
+        case '+':
+            $currentAnswer = $num1 + $num2;
+            break;
+        case '-':
+            $currentAnswer = $num1 - $num2;
+            break;
+        case '*':
+            $currentAnswer = $num1 * $num2;
+            break;
+    }
+    return $currentAnswer;
+}
+
+/**
+ * Function run game
+ *
+ * @return void
+ */
+function runCalc()
+{
+    $getCalcData = function () {
+        $operation = OPERATIONS[array_rand(OPERATIONS)];
+        $num1 = mt_rand(1, MAX_RANDOM_VALUE);
+        $num2 = mt_rand(1, MAX_RANDOM_VALUE);
+
+        $question = "$num1 $operation $num2";
+        $currentAnswer = strval(calculate($num1, $num2, $operation));
+        
+        $gameData = [];
+        $gameData['currentAnswer'] = $currentAnswer;
+        $gameData['question'] = $question;
+        return $gameData;
+    };
+
+    run($getCalcData, DESCRIPTION);
+}
